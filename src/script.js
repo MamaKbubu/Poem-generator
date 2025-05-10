0a6b33035351ea130504747738e1621ec94bb320
MamaKbubu <maboelatk@gmail.com>
2025-05-10 23:22:36 +0200

    activity 4

--- src/script.js
+++ src/script.js
@@ -7,19 +7,20 @@ function displayPoem(response) {
   });
 }
 
+let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "449918400f26bobdf44b32689d6tafaa";
+let prompt = `Generate a poem about ${instructionsInput.value}`;
+let context = "Write a poem about a rose and a tomb";
+let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+axios.get(apiURL).then(displayPoem);
+
 function generatePoem(event) {
   event.preventDefault();
 
   //build the API URL
   // Make a call
   // Display the generated poem
-  let instructionsInput = document.querySelector("#user-instructions");
-  let apiKey = "449918400f26bobdf44b32689d6tafaa";
-  let prompt = "Generate a poem about flowers";
-  let context = "Write a poem about a rose and a tomb";
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
-
-  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
