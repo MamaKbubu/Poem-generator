664db2e68f82316b8f8002f130707d133e35ef93
MamaKbubu <maboelatk@gmail.com>
2025-05-10 22:25:23 +0200

    activity 4

--- src/script.js
+++ src/script.js
@@ -1,6 +1,36 @@
 function generatePoem(event) {
   event.preventDefault();
 
+  //build the API URL
+  // Make a call
+  // Display the generated poem
+
+  let apiKey = "449918400f26bobdf44b32689d6tafaa";
+  let prompt = "";
+  let context = "Write a poem about a rose and a tomb";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  axios.get(apiURL).then(displayPoem);
+}
+function displayPoem(response) {
+  let poem = response.data.poem;
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = poem;
+  poemElement.classList.remove("hidden");
+  poemElement.classList.add("poem");
+  let poemTitle = document.querySelector("#poem-title");
+  poemTitle.innerHTML = "La tombe dit à la rose";
+  poemTitle.classList.remove("hidden");
+  poemTitle.classList.add("poem-title");
+  let poemAuthor = document.querySelector("#poem-author");
+  poemAuthor.innerHTML = "by SheCodes AI";
+  poemAuthor.classList.remove("hidden");
+  poemAuthor.classList.add("poem-author");
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.classList.add("hidden");
+  let poemFormButton = document.querySelector("#poem-generator-button");
+  poemFormButton.classList.add("hidden");
+
   new Typewriter("#poem", {
     strings: "La tombe dit à la rose",
     autoStart: true,
